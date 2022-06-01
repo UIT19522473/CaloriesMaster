0a5a97cb8cd8b92f5f6fad4b108d0944268fcab9
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2022-06-01 17:12:38 +0700

    commit 2nd 1/6
    Hoàn thành modal trong trang home

--- demo/test/src/Component/cpnHome/Body/Modal.js
+++ demo/test/src/Component/cpnHome/Body/Modal.js
@@ -3,6 +3,7 @@ import React from 'react';
 import COLORS from '../../../Constraint/Color';
 import LinearGradient from 'react-native-linear-gradient';
 import Feather from 'react-native-vector-icons/Feather';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 const ModalScreen = props => {
   return (
     <View style={{flex: 1}}>
@@ -12,112 +13,155 @@ const ModalScreen = props => {
         transparent
         visible={props.modalWeight}
         onRequestClose={() => props.setModalWeight(false)}>
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => props.setModalWeight(false)}
+        <View
+          // activeOpacity={1}
+          // onPress={() => props.setModalWeight(false)}
           style={{
-            backgroundColor: '#00000099',
+            // backgroundColor: '#00000099',
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
+            // paddingHorizontal: 12,
           }}>
-          <View
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => props.setModalWeight(false)}
             style={{
-              paddingHorizontal: 36,
-              backgroundColor: COLORS.white,
-              width: '95%',
-              borderRadius: 12,
-              paddingVertical: 12,
+              backgroundColor: '#00000099',
+              flex: 1,
+              width: '100%',
+              justifyContent: 'center',
               alignItems: 'center',
             }}>
-            <Text
-              style={{fontSize: 18, color: COLORS.black, fontWeight: 'bold'}}>
-              Theo dõi cân nặng
-            </Text>
+            {/* <Text>hsahashsa</Text> */}
+          </TouchableOpacity>
+
+          <View
+            style={{
+              width: '100%',
+              backgroundColor: '#00000099',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingHorizontal: 8,
+            }}>
             <View
               style={{
-                marginVertical: 24,
-                paddingHorizontal: 16,
-                flexDirection: 'row',
+                paddingHorizontal: 36,
+                backgroundColor: COLORS.white,
                 width: '100%',
-                justifyContent: 'space-between',
+                borderRadius: 12,
+                paddingVertical: 12,
+                alignItems: 'center',
+                marginHorizontal: 100,
               }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: COLORS.black,
+                  fontWeight: 'bold',
+                }}>
+                Theo dõi cân nặng
+              </Text>
               <View
                 style={{
-                  backgroundColor: COLORS.silver,
-                  // padding: 4,
-                  borderRadius: 50,
-                  height: 26,
-                  width: 26,
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  marginVertical: 24,
+                  paddingHorizontal: 16,
+                  flexDirection: 'row',
+                  width: '100%',
+                  justifyContent: 'space-between',
                 }}>
-                <Feather name="minus" color={COLORS.grey} size={20}></Feather>
+                <View
+                  style={{
+                    backgroundColor: COLORS.silver,
+                    // padding: 4,
+                    borderRadius: 50,
+                    height: 26,
+                    width: 26,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Feather name="minus" color={COLORS.grey} size={20}></Feather>
+                </View>
+
+                <Text style={{fontSize: 24, fontWeight: 'bold'}}>70 kg</Text>
+                <View
+                  style={{
+                    backgroundColor: COLORS.silver,
+                    // padding: 4,
+                    borderRadius: 50,
+                    height: 26,
+                    width: 26,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Feather name="plus" color={COLORS.grey} size={20}></Feather>
+                </View>
               </View>
 
-              <Text style={{fontSize: 24, fontWeight: 'bold'}}>70 kg</Text>
               <View
                 style={{
-                  backgroundColor: COLORS.silver,
-                  // padding: 4,
-                  borderRadius: 50,
-                  height: 26,
-                  width: 26,
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                  paddingHorizontal: 18,
+                  marginTop: 12,
                 }}>
-                <Feather name="plus" color={COLORS.grey} size={20}></Feather>
-              </View>
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                width: '100%',
-                paddingHorizontal: 18,
-                marginTop: 12,
-              }}>
-              <TouchableOpacity
-                style={{
-                  borderColor: 'orange',
-                  borderWidth: 1,
-                  borderRadius: 15,
-                  paddingHorizontal: 30,
-                  paddingVertical: 8,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text
-                  style={{fontSize: 16, fontWeight: 'bold', color: 'orange'}}>
-                  Bỏ qua
-                </Text>
-              </TouchableOpacity>
-
-              <LinearGradient
-                style={{borderRadius: 15}}
-                start={{x: 0.2, y: 0.5}}
-                end={{x: 1, y: 0.5}}
-                colors={['orange', 'red']}>
                 <TouchableOpacity
                   style={{
+                    borderColor: 'orange',
+                    borderWidth: 1,
+                    borderRadius: 15,
                     paddingHorizontal: 30,
                     paddingVertical: 8,
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}>
                   <Text
                     style={{
-                      alignItems: 'center',
-                      color: COLORS.white,
+                      fontSize: 16,
                       fontWeight: 'bold',
-                      fontSize: 17,
+                      color: 'orange',
                     }}>
-                    Cập nhật
+                    Bỏ qua
                   </Text>
                 </TouchableOpacity>
-              </LinearGradient>
+
+                <LinearGradient
+                  style={{borderRadius: 15}}
+                  start={{x: 0.2, y: 0.5}}
+                  end={{x: 1, y: 0.5}}
+                  colors={['orange', 'red']}>
+                  <TouchableOpacity
+                    style={{
+                      paddingHorizontal: 30,
+                      paddingVertical: 8,
+                    }}>
+                    <Text
+                      style={{
+                        alignItems: 'center',
+                        color: COLORS.white,
+                        fontWeight: 'bold',
+                        fontSize: 17,
+                      }}>
+                      Cập nhật
+                    </Text>
+                  </TouchableOpacity>
+                </LinearGradient>
+              </View>
             </View>
           </View>
-        </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => props.setModalWeight(false)}
+            style={{
+              backgroundColor: '#00000099',
+              flex: 1,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}></TouchableOpacity>
+        </View>
       </Modal>
     </View>
   );
--- demo/test/src/Screen/Login.js
+++ demo/test/src/Screen/Login.js
@@ -2,20 +2,30 @@ import React from 'react';
 import {View, Button, Text, StyleSheet, ImageBackground} from 'react-native';
 
 import GoogleSignInButton from '../Component/cpnLogin/Body/GoogleSignInButton';
-import FacebookSignInButton from '../Component/cpnLogin/Body/FacebookSignInButton'
-import EmailSignInButton from '../Component/cpnLogin/Body/EmailSignInButton'
+import FacebookSignInButton from '../Component/cpnLogin/Body/FacebookSignInButton';
+import EmailSignInButton from '../Component/cpnLogin/Body/EmailSignInButton';
 import background_image from '../Image/background_login.png';
-
 
 const Login = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <ImageBackground source={background_image} resizeMode="cover" style={styles.image}>
-          <EmailSignInButton text = "Đăng nhập bằng Email"></EmailSignInButton>
-          <GoogleSignInButton text = "Đăng nhập bằng Google" style = {styles.signInButton}></GoogleSignInButton>
-          <FacebookSignInButton text = "Đăng nhập bằng Facebook" ></FacebookSignInButton>
+      <ImageBackground
+        source={background_image}
+        resizeMode="cover"
+        style={styles.image}>
+        <EmailSignInButton text="Đăng nhập bằng Email"></EmailSignInButton>
+        <GoogleSignInButton
+          text="Đăng nhập bằng Google"
+          style={styles.signInButton}></GoogleSignInButton>
+        <FacebookSignInButton text="Đăng nhập bằng Facebook"></FacebookSignInButton>
       </ImageBackground>
-  </View>
+
+      <Button
+        title="MainPage >>"
+        onPress={() => {
+          navigation.navigate('MainBottomTab');
+        }}></Button>
+    </View>
   );
 };
 
@@ -25,16 +35,10 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  signInButton: {
-  }
+  signInButton: {},
 });
 
-
-
 export default Login;
-// onPress={() => {
-//   navigation.navigate('MainBottomTab');
-// }}
